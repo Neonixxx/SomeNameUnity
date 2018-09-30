36454bd4dde80d147e648fcb07306a841f910ad2
Daniil Perminov <q3zxc@mail.ru>
2018-09-30 16:31:46 +0400

    Сделал общий объект с методами для кнопок

---
+++ Assets/Scripts/SomeName/Buttons.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Buttons : MonoBehaviour
+{
+    public void LoadScene(string sceneName)
+        => SceneManager.LoadScene(sceneName);
+}
